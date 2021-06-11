a70fe5c416fcea28911e2d801c255d7c6f1c2cbb
FIRST_NAME LAST_NAME <rujal.sapkota@aratitech.org>
2021-06-11 17:18:02 +0545

    modified table and restored the index.js page to its original form

--- pages/index.js
+++ pages/index.js
@@ -1,93 +1,10 @@
-import PaginateTable from 'components/modules/PaginateTable'
 import React from 'react'
+import Button from 'elements/Button'
 
-const data = [
-  {
-    key: '1',
-    name: 'John Brown',
-    age: 32,
-    address: 'New York No. 1 Lake Park',
-    edit: <button onClick={() => console.log(1)}>Edit</button>
-  },
-  {
-    key: '2',
-    name: 'Jim Green',
-    age: 42,
-    address: 'London No. 1 Lake Park',
-    edit: <button onClick={() => console.log(2)}>Edit</button>
-  },
-  {
-    key: '3',
-    name: 'Joe Black',
-    age: 32,
-    address: 'Sidney No. 1 Lake Park',
-    edit: <button onClick={() => console.log(3)}>Edit</button>
-  },
-  {
-    key: '4',
-    name: 'Joe Black',
-    age: 32,
-    address: 'Sidney No. 1 Lake Park',
-    edit: <button onClick={() => console.log(4)}>Edit</button>
-  },
-  {
-    key: '5',
-    name: 'Joe Black',
-    age: 32,
-    address: 'Sidney No. 1 Lake Park',
-    edit: <button onClick={() => console.log(5)}>Edit</button>
-  },
-  {
-    key: '6',
-    name: 'Joe Black',
-    age: 32,
-    address: 'Sidney No. 1 Lake Park',
-    edit: <button onClick={() => console.log(6)}>Edit</button>
-  },
-  {
-    key: '7',
-    name: 'Joe Black',
-    age: 32,
-    address: 'Sidney No. 1 Lake Park',
-    edit: <button onClick={() => console.log(7)}>Edit</button>
-  },
-  {
-    key: '8',
-    name: 'Joe Black',
-    age: 32,
-    address: 'Sidney No. 1 Lake Park',
-    edit: <button onClick={() => console.log(8)}>Edit</button>
-  },
-  {
-    key: '9',
-    name: 'Joe Black',
-    age: 32,
-    address: 'Sidney No. 1 Lake Park',
-    edit: <button onClick={() => console.log(9)}>Edit</button>
-  },
-  {
-    key: '10',
-    name: 'Joe Black',
-    age: 32,
-    address: 'Sidney No. 1 Lake Park',
-    edit: <button onClick={() => console.log(10)}>Edit</button>
-  },
-  {
-    key: '11',
-    name: 'Joe Black',
-    age: 32,
-    address: 'Sidney No. 1 Lake Park',
-    edit: <button onClick={() => console.log(11)}>Edit</button>
-  },
-]
 function Home() {
-
   return (
     <div>
-      <PaginateTable
-        data={data}
-        tableBodyStyle={{ background: "green", color: "yellow" }}
-      />
+      This is homepage<Button></Button>
     </div>
   )
 }
