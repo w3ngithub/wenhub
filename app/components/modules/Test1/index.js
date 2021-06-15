f5134040035713129b7a012778335ec830e5e579
FIRST_NAME LAST_NAME <rujal.sapkota@aratitech.org>
2021-06-15 11:52:56 +0545

    added test1 module for testing component

---
+++ app/components/modules/Test1/index.js
@@ -0,0 +1,37 @@
+import React from 'react'
+import PaginateTable from 'components/modules/PaginateTable'
+import Modal from 'components/elements/Modal'
+
+const Test1 = () => {
+    const [visible, setVisible] = React.useState(true)
+    return (
+        <div>
+            <div>
+                <h1>Table</h1>
+                <PaginateTable />
+            </div>
+            <div>
+                <h1>Modal</h1>
+                <button onClick={() => setVisible(true)}>Modal</button>
+
+                <Modal
+                    visible={visible}
+                    handleOk={() => setVisible(false)}
+                    handleCancel={() => setVisible(false)}
+                    bodyStyle={{ color: "purple", background:"red" }}
+                    confirmText="Delete"
+                    cancelText="No"
+                    headerBackground="white"
+                    headerFontSize="1rem"
+                    headerTextColor="blue"
+                    footerBackground="white"
+                    variant="medium"
+                >
+                    These are the contents
+                </Modal>
+            </div>
+        </div >
+    )
+}
+
+export default Test1
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import Button from 'components/elements/Button'
+import Test1 from 'modules/Test1'
 
 function Home() {
   return (
@@ -8,6 +9,7 @@ function Home() {
       <div>
         <Button />
       </div>
+      <Test1 />
     </div>
   )
 }
