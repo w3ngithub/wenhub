f4c6ea134ad6280c0d8cff23fb321a90d3d3fd51
FIRST_NAME LAST_NAME <rujal.sapkota@aratitech.org>
2021-06-17 12:35:57 +0545

    Solved some eslint issues

--- app/components/modules/PaginateTable/index.js
+++ app/components/modules/PaginateTable/index.js
@@ -3,78 +3,89 @@ import Table from 'components/elements/Table'
 import { Paginate, listData } from 'components/elements/Pagination'
 
 const PaginateTable = ({ columns, data, tableBodyStyle }) => {
+  const [pageNumber, setPageNumber] = React.useState(1)
+  const [postPerPage, setPostPerPage] = React.useState(5)
 
-    const [pageNumber, setPageNumber] = React.useState(1)
-    const [postPerPage, setPostPerPage] = React.useState(5)
+  const changePage = (pageNo, pageSize) => {
+    setPageNumber(pageNo)
+    setPostPerPage(pageSize)
+  }
+  const finalData = listData(data, postPerPage, pageNumber)
 
-    const changePage = (pageNo, pageSize) => {
-        setPageNumber(pageNo)
-        setPostPerPage(pageSize)
-    }
-    const finalData = listData(data, postPerPage, pageNumber)
-
-    return (
-        <div>
-            <Table
-                columns={columns}
-                data={finalData}
-                tableBodyStyle={tableBodyStyle}
-            />
-            <div style={{ marginTop: 10 }}></div>
-            <Paginate
-                handlePageChange={changePage}
-                length={data.length}
-                pageSize={postPerPage}
-            />
-        </div>
-    )
+  return (
+    <div>
+      <Table
+        columns={columns}
+        data={finalData}
+        tableBodyStyle={tableBodyStyle}
+      />
+      <div style={{ marginTop: 10 }}></div>
+      <Paginate
+        handlePageChange={changePage}
+        length={data.length}
+        pageSize={postPerPage}
+      />
+    </div>
+  )
 }
 
 PaginateTable.defaultProps = {
-    tableBodyStyle: {
-        background: "#ddd"
+  tableBodyStyle: {
+    background: '#ddd',
+  },
+  columns: [
+    {
+      title: 'Name',
+      keyIndex: 'name',
     },
-    columns: [
-        {
-            title: "Name",
-            keyIndex: "name",
-        },
-        {
-            title: "Age",
-            keyIndex: "age",
-        },
-        {
-            title: "Address",
-            keyIndex: "address"
-        },
-        {
-            title: "Actions",
-            keyIndex: "edit",
-        }
-    ],
-    data: [
-        {
-            key: '1',
-            name: 'John Brown',
-            age: 32,
-            address: 'New York No. 1 Lake Park',
-            edit: <button onClick={() => console.log(1)}>Edit</button>
-        },
-        {
-            key: '2',
-            name: 'Jim Green',
-            age: 42,
-            address: 'London No. 1 Lake Park',
-            edit: <button onClick={() => console.log(2)}>Edit</button>
-        },
-        {
-            key: '3',
-            name: 'Joe Black',
-            age: 32,
-            address: 'Sidney No. 1 Lake Park',
-            edit: <button onClick={() => console.log(3)}>Edit</button>
-        }
-    ]
+    {
+      title: 'Age',
+      keyIndex: 'age',
+    },
+    {
+      title: 'Address',
+      keyIndex: 'address',
+    },
+    {
+      title: 'Actions',
+      keyIndex: 'edit',
+    },
+  ],
+  data: [
+    {
+      key: '1',
+      name: 'John Brown',
+      age: 32,
+      address: 'New York No. 1 Lake Park',
+      edit: (
+        <button type="button" onClick={() => {}}>
+          Edit
+        </button>
+      ),
+    },
+    {
+      key: '2',
+      name: 'Jim Green',
+      age: 42,
+      address: 'London No. 1 Lake Park',
+      edit: (
+        <button type="button" onClick={() => {}}>
+          Edit
+        </button>
+      ),
+    },
+    {
+      key: '3',
+      name: 'Joe Black',
+      age: 32,
+      address: 'Sidney No. 1 Lake Park',
+      edit: (
+        <button type="button" onClick={() => {}}>
+          Edit
+        </button>
+      ),
+    },
+  ],
 }
 
 export default PaginateTable
