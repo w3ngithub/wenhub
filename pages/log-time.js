714034cc3797c6f7540e39bbc6362cf3287a5417
Ashok Ganika <ganika.ashok@webexpertsnepal.com>
2021-07-06 05:18:55 +0000

    Resolve "Create a log time page"

---
+++ pages/log-time.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import LogTime from 'components/templates/LogTime'
+
+function LogTimePage() {
+  return (
+    <>
+      <LogTime />
+    </>
+  )
+}
+
+export default LogTimePage
